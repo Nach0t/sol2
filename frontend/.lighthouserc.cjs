f44f2b2520b6637c8739bf10d1736174a0ed4f2b
Ignacio Rehbein <78046088+Nach0t@users.noreply.github.com>
2025-05-28 11:45:52 -0400

    Update .lighthouserc.cjs

--- frontend/.lighthouserc.cjs
+++ frontend/.lighthouserc.cjs
@@ -2,7 +2,7 @@ module.exports = {
     ci: {
       collect: {
         startServerCommand: 'npm run preview',
-        url: ['http://localhost:5173/'],
+        url: ['http://localhost:4173/'],
         numberOfRuns: 3
       },
       assert: {
