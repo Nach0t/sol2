e6b388741d3e9e8c65023dbd96dbf4de752eff12
Ignacio Rehbein <78046088+Nach0t@users.noreply.github.com>
2025-06-17 13:00:08 -0400

    update

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,29 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var rankingRouter = require('./routes/ranking');  
+// Rutas
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const rankingRouter = require('./routes/ranking');
+const registerRouter = require('./routes/register'); // Si agregas register
 
-var app = express();
+const app = express();
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rutas principales
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/ranking', rankingRouter);              
+app.use('/ranking', rankingRouter);
+app.use('/api', registerRouter); // para POST /api/register
 
 module.exports = app;
